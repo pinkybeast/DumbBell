5114c160e70b8f8615ec8f0b3c8bd6a1def79a6a
aclrdhv <marcellvirgiano@gmail.com>
2024-07-04 16:07:30 +0700

    refactor: change title name

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
     include_once('./config/config.php');
     session_start();
-
+    
     $customer_id = $_SESSION['customer_id'];
-
+    
     if(!isset($customer_id)){
         header('location:login.php');
     }
@@ -65,7 +65,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
+        <title>DumbBell</title>
 
         <!--font awesome cdnjs link-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
