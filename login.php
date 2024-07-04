71362d5a374cdb353e3989430c3a16aa04df5d36
aclrdhv <marcellvirgiano@gmail.com>
2024-07-04 16:05:23 +0700

    refactor: change title name

--- login.php
+++ login.php
@@ -37,7 +37,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>
+        <title>Login | DumbBell</title>
 
         <!--font awesome cdnjs link-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
--- orders.php
+++ orders.php
@@ -16,7 +16,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Orders</title>
+        <title>Orders | DumbBell</title>
 
         <!--font awesome cdnjs link-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
@@ -38,7 +38,7 @@
 
         <section class="placed-orders">
 
-            <h1 class="title"><span>&#9829;</span> placed orders <span>&#9829;</span></h1>
+            <h1 class="title">placed orders</h1>
 
             <div class="box-container">
 
