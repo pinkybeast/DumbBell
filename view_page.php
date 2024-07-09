61f35b63ce8b0b2162244a70c49f7ae35af36408
aclrdhv <marcellvirgiano@gmail.com>
2024-07-09 19:20:06 +0700

    refactor: fixing view page

--- view_page.php
+++ view_page.php
@@ -1,60 +1,6 @@
 <?php
     include_once('./config/config.php');
-    session_start();
-
-    $customer_id = $_SESSION['customer_id'];
-
-    if(!isset($customer_id)){
-        header('location:login.php');
-    }
-
-    if(isset($_POST['add_to_wishlist'])){
-        $product_id = $_POST['product_id'];
-        $product_name = $_POST['product_name'];
-        $product_price = $_POST['product_price'];
-        $product_color = $_POST['product_color'];
-        $product_category = $_POST['product_category'];
-        $product_image = $_POST['product_image'];
-        
-        $sql_check_wishlist_num = mysqli_query($conn, "SELECT * FROM wishlist WHERE customer_id = '$customer_id' AND name = '$product_name' AND color = '$product_color'") or die('Query Failed');
-        $sql_check_cart_num = mysqli_query($conn, "SELECT * FROM cart WHERE customer_id = '$customer_id' AND name = '$product_name' AND color = '$product_color'") or die('Query Failed');
-
-        if(mysqli_num_rows($sql_check_wishlist_num) > 0){
-            $message[] = 'Product already added to wishlist';
-        }
-        else if(mysqli_num_rows($sql_check_cart_num) > 0){
-            $message[] = 'Product already added to cart';
-        }
-        else{
-            mysqli_query($conn, "INSERT INTO wishlist(customer_id, pid, name, price, color, category, image) VALUES('$customer_id', '$product_id', '$product_name', '$product_price', '$product_color', '$product_category', '$product_image')") or die('Query Failed');
-            $message[] = 'Added product to wishlist successfully!';
-        }
-    }
-
-    if(isset($_POST['add_to_cart'])){
-        $product_id = $_POST['product_id'];
-        $product_name = $_POST['product_name'];
-        $product_price = $_POST['product_price'];
-        $product_category = $_POST['product_category'];
-        $product_quantity = $_POST['product_quantity'];
-        $product_image = $_POST['product_image'];
-        
-        $sql_check_cart_num = mysqli_query($conn, "SELECT * FROM cart WHERE customer_id = '$customer_id' AND name = '$product_name' AND color = '$product_color'") or die('Query Failed');
-
-        if(mysqli_num_rows($sql_check_cart_num) > 0){
-            $message[] = 'Product already added to cart';
-        }
-        else{
-
-            $sql_check_wishlist_num = mysqli_query($conn, "SELECT * FROM wishlist WHERE customer_id = '$customer_id' AND name = '$product_name' AND color = '$product_color'") or die('Query Failed');
-
-            if(mysqli_num_rows($sql_check_wishlist_num) > 0){
-                mysqli_query($conn, "DELETE FROM wishlist WHERE customer_id = '$customer_id' AND name = '$product_name' AND color = '$product_color'") or die('Query Failed');
-            }
-            mysqli_query($conn, "INSERT INTO cart(customer_id, pid, name, price, color, category, quantity, image) VALUES('$customer_id', '$product_id', '$product_name', '$product_price', '$product_color', '$product_category', '$product_quantity', '$product_image')") or die('Query Failed');
-            $message[] = 'Added product to cart successfully!';
-        }
-    }
+    include_once('./header.php');
 
     if(isset($_GET['pid'])){
         $pid = $_GET['pid'];
@@ -80,10 +26,6 @@
     </head>
 
     <body>
-
-    <?php  
-        include_once('./header.php');
-    ?>
         <section class="quick-view">
             <h1 class="title"> product details </h1>
             <?php
