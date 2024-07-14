2567fbc9fb174a46915ea03b08395d2f986d57e2
Haikal <mhaikalhalim@gmail.com>
2024-07-15 01:30:58 +0700

    Aman

--- login.php
+++ login.php
@@ -58,14 +58,6 @@
 
     <body>
 
-    <?php
-        if(isset($message)){
-            foreach($message as $message){
-                echo '<div class="message"><span>'.$message.'</span><i class="fas fa-times" onclick="this.parentElement.remove();"></i></div>';
-            }
-        }
-    ?>
-
         <div class="form-container">
             <form action="" method="post">
                 <h3>login now</h3>
